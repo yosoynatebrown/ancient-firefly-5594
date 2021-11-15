627a494f64fdc2bb4eb1db5ff9e4bd340cf7a9e5
Nate Brown <77140183+yosoynatebrown@users.noreply.github.com>
2021-11-15 12:01:50 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :teams,             only: [:index]
   resources :competitions,      only: [:show]
-  resources :team_competitions, only: [:create]
 
   post '/competitions/:id/teams', to: 'team_competitions#create'
 end
